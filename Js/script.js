474392dce7a520a062b722b46cf2fed06b9f3878
Viktor <dzyamkorv@gmail.com>
2022-02-04 20:07:28 +0100

    first commid

---
+++ Js/script.js
@@ -0,0 +1,11 @@
+"use strict"
+// const result = confirm("Are you here?");
+// console.log(result);
+// const answer = +prompt('Вам есть 18?', '18');
+// console.log(answer + 5);
+const answers = [];
+answers[0] = ("Как тебя зовут?", "");
+answers[1] = prompt("Сколько тебе лет?", "");
+answers[2] = prompt("Кто ты по професии?", "");
+
+document.write(answers);
