91b7827897c732a24d0aa1fe1472497e792f6f63
Viktor <dzyamkorv@gmail.com>
2022-02-05 17:25:12 +0100

    Первое приложение Джава

--- Js/script.js
+++ Js/script.js
@@ -1,11 +1,21 @@
 "use strict"
-// const result = confirm("Are you here?");
-// console.log(result);
-// const answer = +prompt('Вам есть 18?', '18');
-// console.log(answer + 5);
-const answers = [];
-answers[0] = ("Как тебя зовут?", "");
-answers[1] = prompt("Сколько тебе лет?", "");
-answers[2] = prompt("Кто ты по професии?", "");
 
-document.write(answers);
+const number0fFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+
+const personaMovieDB = {
+    count: number0fFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+const a = prompt('Сколько фильмов вы уже посмотрели?',''),
+      b = prompt('На сколько оцените его?',''),
+      c = prompt('Сколько фильмов вы уже посмотрели?',''),
+      d = prompt('На сколько оцените его?','');
+
+personaMovieDB.movies[a] = b;
+personaMovieDB.movies[c] = d;
+
+console.log(personaMovieDB);
